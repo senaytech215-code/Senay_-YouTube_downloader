21728493a0e68c7d570b255216197a975bc148c9
senaytech215-code <senaytech215@gmail.com>
2026-05-13 17:25:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,22 @@ import os
 import telebot
 from flask import Flask, request
 
-# Token ከ Render Variables ይቀበላል
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def handle_webhook():
+# ይህ ክፍል ቴሌግራም መልእክት ሲልክ የሚቀበለው ነው
+@app.route('/', methods=['POST', 'GET'])
+def webhook():
     if request.method == 'POST':
-        json_string = request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
+        update = telebot.types.Update.de_json(request.get_data().decode('utf-8'))
         bot.process_new_updates([update])
-        return "OK", 200
-    return "ቦቱ ዝግጁ ነው!", 200
+        return 'ok', 200
+    return 'Server is Running', 200
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, "ሰላም ሰናይ! አሁን ግንኙነቱ ሰርቷል።")
+    bot.reply_to(message, "አሁን ሰርቷል! መልእክትህ ደርሶኛል።")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
