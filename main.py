517bdff148f413d3c78752df1c182cf8087758f6
senaytech215-code <senaytech215@gmail.com>
2026-05-13 17:00:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import os
 import telebot
 from flask import Flask, request
 
-# Render ላይ ያስገባኸውን Token እዚህ ጋር ይቀበላል
+# Render Environment Variables
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "ቦቱ አሁን በቀላሉ እየሰራ ነው!"
+    return "ቦቱ በሰላም እየሰራ ነው!"
 
-@app.route('/' + BOT_TOKEN, methods=['POST'])
+# ዋናው ማስተካከያ እዚህ ጋር ነው - Path መጠቀሙን እናጥፋው
+@app.route('/', methods=['POST'])
 def getMessage():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return "!", 200
 
-# የላክነውን መልእክት መልሶ እንዲልክልን (Echo)
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, f"የላክኸው መልእክት፦ {message.text}")
+    bot.reply_to(message, f"የላክኸው መልእክት ደርሶኛል፦ {message.text}")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
